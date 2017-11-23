7f9c964bebeebef5abf97b513fde554399fc33ca
Juan Andrés <amorenocb@gmail.com>
2017-11-23 16:28:03 -0300

    Added glove to get all the terms related to the search terms.

--- contentBasedSearch/settings.py
+++ contentBasedSearch/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import glove
+from gensim.models import word2vec
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'searcher'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'contentBasedSearch.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+glove_instance = glove.Glove()
+
+TERM_SEARCH_ENGINE = glove_instance.load_stanford(filename="vectorsGloveLight.txt")
--- searcher/views.py
+++ searcher/views.py
@@ -1,3 +1,36 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.template.loader import render_to_string
+from django.views import View
+from django.conf import settings
+
+class Index(View):
+    template_name = 'searcher/index.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+    def post(self, request):
+        search_terms = request.POST.get('search_terms')
+        top_n = int(request.POST.get('top_n'))
+        search_terms_array = search_terms.split(" ")
+        similar_terms = []
+        for term in search_terms_array:
+            results = settings.TERM_SEARCH_ENGINE.most_similar(term.lower(), top_n+1)
+            similar_terms.extend(results)
+
+        used_terms = []
+        for word, prob in similar_terms:
+            used_terms.append({'word':word, 'prob':prob})
+
+        term_results_template = render_to_string('searcher/term_results.html', {'used_terms': used_terms})
+        images_results_template = render_to_string('searcher/results.html')
+
+        context = {'term_results' : term_results_template, 'image_results' : images_results_template}
+        return JsonResponse(context)
+
+
+
+
